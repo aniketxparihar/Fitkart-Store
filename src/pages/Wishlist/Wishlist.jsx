7396dc2dfd0b65f2e1008eca42c6fea1516feb79
Aniket Singh Parihar <aniket.parihar1718@gmail.com>
2022-04-07 14:26:19 +0530

    Feat: Add Wishlist Component
    Pages Added
    Wishlist
    Components Added
    WishlistCard

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,20 +1,41 @@
-import React,{useState,useEffect} from 'react'
-import "./Wishlist.css"
-import { useWishlist } from '../../Context/wishlist-context';
-import getDiscount from '../../components/Discount/Discount';
-import { useAuth } from '../../Context/auth-Context';
+import React, { useState, useEffect } from "react";
+import "./Wishlist.css";
+import { useWishlist } from "../../Context/wishlist-context";
+import getDiscount from "../../components/Discount/Discount";
+import { useAuth } from "../../Context/auth-Context";
 import axios from "axios";
-import ProductCard from '../../components/ProductCard/ProductCard';
-import WishlistCard from '../../components/WishlistCard/WishlistCard';
-
+import ProductCard from "../../components/ProductCard/ProductCard";
+import WishlistCard from "../../components/WishlistCard/WishlistCard";
 
 const Wishlist = () => {
-  
+  const { authToken } = useAuth();
+  const { wishlistItems, setWishlistItems } = useWishlist();
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get("/api/user/wishlist", {
+        headers: {
+          authorization: authToken,
+        },
+      });
+      setWishlistItems(response.data.wishlist);
+    })();
+  }, []);
   return (
-      <div className="wishlist__main">
-         
-      </div>
+    <div className="wishlist__main">
+      <div className="wishlist__heading">Your Wishlist</div>
+      {wishlistItems.length > 0 ? (
+        <div className="wishlist__products">
+          {wishlistItems.map((product) => {
+            return <WishlistCard product={product} />;
+          })}
+        </div>
+      ) : (
+        <div className="wishlist__empty">
+          Nothing Here :( add some items and Come Back
+        </div>
+      )}
+    </div>
   );
-}
+};
 
-export default Wishlist
+export default Wishlist;
